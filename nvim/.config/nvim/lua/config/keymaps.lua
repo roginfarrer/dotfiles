39507bd52d02087387a2088684510430d7a18047
Rogin Farrer <rogin@roginfarrer.com>
2026-04-12 14:14:57 -0700

    better <c-g>

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -92,3 +92,29 @@ map {
 	end,
 	desc = 'Git conflicts to quickfix',
 }
+
+map {
+	'<c-g>',
+	function()
+		-- Taken from https://github.com/wsdjeg/ctrlg.nvim
+		local pwd = vim.fn.getcwd()
+		local project_name
+		if vim.fn.isdirectory '.git' == 1 then
+			project_name = string.format('[%s]', vim.fn.fnamemodify(pwd, ':t'))
+		else
+			project_name = ''
+		end
+
+		local file = vim.fn.fnamemodify(vim.fn.expand '%', ':.')
+
+		local messages = {}
+
+		if #project_name > 0 then
+			vim.list_extend(messages, { { project_name, 'Constant' }, { '  >>  ', 'WarningMsg' } }, 1, 2)
+		end
+
+		vim.list_extend(messages, { { pwd, 'Special' }, { '  >>  ', 'WarningMsg' }, { file, 'Directory' } }, 1, 3)
+		vim.api.nvim_echo(messages, false, {})
+	end,
+	desc = 'Better <ctrl-g>',
+}
