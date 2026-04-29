16ea8b65ea2a711ed17bd5f984c31e3ed03d4dd5
Rogin Farrer <rfarrer@etsy.com>
2026-04-29 15:05:55 -0700

    update aerospace

--- fish/.config/fish/functions/brew-upgrade.fish
+++ fish/.config/fish/functions/brew-upgrade.fish
@@ -1,3 +1,6 @@
 function brew-upgrade -d "Use `fzf` to interactively select and upgrade homebrew pkgs"
+    # If not run in a while, it logs some fetching messages
+    # So we run it first to get those out of the way
+    brew outdated >/dev/null
     brew outdated | fzf -m --header='Tab to select, Enter to upgrade' | xargs brew upgrade
 end
