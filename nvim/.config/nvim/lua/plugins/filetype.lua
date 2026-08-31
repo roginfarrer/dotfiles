baf0997e02b61a205d787bbc60c5b435f4ef29ef
Rogin Farrer <rogin@roginfarrer.com>
2026-08-31 16:36:36 +0000

    tmux nvim, lazy-tmux

--- nvim/.config/nvim/lua/plugins/filetype.lua
+++ nvim/.config/nvim/lua/plugins/filetype.lua
@@ -1,4 +1,5 @@
 return {
 	{ 'mustache/vim-mustache-handlebars', enabled = true },
 	{ 'davidmh/mdx.nvim', dependencies = { 'nvim-treesitter/nvim-treesitter' }, lazy = false },
+	{ 'tmux-plugins/vim-tmux', ft = 'tmux' },
 }
