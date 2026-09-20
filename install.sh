7ae29decc22c8dc1f48abf5edd805b85174d34c9
Rogin Farrer <rogin@roginfarrer.com>
2026-09-20 11:32:21 -0700

    editorconfig

--- install.sh
+++ install.sh
@@ -19,26 +19,26 @@ else
 fi
 
 case "${SHELL}" in
-*/bash*)
-    if [[ -n "${HOMEBREW_ON_LINUX-}" ]]; then
-        shell_rcfile="${HOME}/.bashrc"
-    else
-        shell_rcfile="${HOME}/.bash_profile"
-    fi
-    ;;
-*/zsh*)
-    if [[ -n "${HOMEBREW_ON_LINUX-}" ]]; then
-        shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zshrc"
-    else
-        shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zprofile"
-    fi
-    ;;
-*/fish*)
-    shell_rcfile="${HOME}/.config/fish/config.fish"
-    ;;
-*)
-    shell_rcfile="${ENV:-"${HOME}/.profile"}"
-    ;;
+    */bash*)
+        if [[ -n "${HOMEBREW_ON_LINUX-}" ]]; then
+            shell_rcfile="${HOME}/.bashrc"
+        else
+            shell_rcfile="${HOME}/.bash_profile"
+        fi
+        ;;
+    */zsh*)
+        if [[ -n "${HOMEBREW_ON_LINUX-}" ]]; then
+            shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zshrc"
+        else
+            shell_rcfile="${ZDOTDIR:-"${HOME}"}/.zprofile"
+        fi
+        ;;
+    */fish*)
+        shell_rcfile="${HOME}/.config/fish/config.fish"
+        ;;
+    *)
+        shell_rcfile="${ENV:-"${HOME}/.profile"}"
+        ;;
 esac
 
 ###
